9a02321a6fd1e7e76c713d675efe7caec8c37ba6
Lukhnos D. Liu <lukhnos@lukhnos.org>
2009-04-15 13:28:43 +0800

    Revert "Revert "Completes RandomPublicPhoto, a Mac sample app.""
    This reverts commit b60be4ed5cf7ca98b813d8e796f0880f9fdf550f.

---
+++ Examples/RandomPublicPhoto/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  RandomPublicPhoto
+//
+//  Created by Lukhnos D. Liu on 4/15/09.
+//  Copyright Lithoglyph Inc. 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
