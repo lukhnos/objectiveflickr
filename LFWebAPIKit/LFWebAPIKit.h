191691353757ac400b3edf0102d5edf7cc04c317
Lukhnos D. Liu <lukhnos@lithoglyph.com>
2009-07-01 03:31:01 +0800

    Adds "site reachability" support to LFWebAPIKit, along with test apps.
    By "reacahble", all these conditions have to be true:
    1. Network interface exists and is properly "up"
    2. Has a valid connectivity
    3. An HTTP request (using HEAD method) can be sent to the destination
    4. A response is sent back from the destination
    
    Test apps for both iPhone and Mac are supplied.

--- LFWebAPIKit/LFWebAPIKit.h
+++ LFWebAPIKit/LFWebAPIKit.h
@@ -28,3 +28,8 @@
 
 #import "LFHTTPRequest.h"
 #import "NSData+LFHTTPFormExtensions.h"
+
+// LFSiteReachability is only available when built aginst OS X 10.5+ or iPhone SDK
+#if MAC_OS_X_VERSION_MIN_REQUIRED > MAC_OS_X_VERSION_10_4
+	#import "LFSiteReachability.h"
+#endif
